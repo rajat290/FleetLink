2b80aa356cec744272770a2f605b7b2cf66efeaf
rajat <rajat290@users.noreply.github.com>
2025-07-31 10:02:39 +0530

    refactor: update navigation links with styled buttons for better UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,21 @@ export default function App() {
   return (
     <div>
       <nav className="bg-indigo-700 text-white py-4 px-6 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">FleetLink 🚚</h1>
+        <h1 className="text-2xl font-bold">FleetLink </h1> 
         <div className="space-x-4">
-          <Link to="/" className="hover:underline">Home</Link>
-          <Link to="/add-vehicle" className="hover:underline">Add Vehicle</Link>
-          <Link to="/search" className="hover:underline">Search & Book</Link>
+          {/* <Link to="/" className="hover:underline">Home</Link> */}
+            <Link
+            to="/add-vehicle"
+            className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg font-semibold shadow-lg"
+          >
+            ➕ Add Vehicle
+          </Link>
+            <Link
+            to="/search"
+            className="bg-green-600 hover:bg-green-700 text-white px-6 py-3 rounded-lg font-semibold shadow-lg"
+          >
+            🚛 Book a Vehicle
+          </Link>
         </div>
       </nav>
 
